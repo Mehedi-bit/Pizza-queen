1dde8089b7ffe8c41d9f86d7e625eb1f7d1fc5b8
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2023-01-27 21:15:46 +0600

    A beautiful navbar designed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,37 @@
+import styles from "@/styles/Navbar.module.css";
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>013 547 245</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="" width="158" height="75" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width="30" height="30" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import Layout from '@/components/Layout'
 import '@/styles/globals.css'
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({ 
+  subsets: ['latin'],
+  weight: ['200', '400', '500', '700']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  )
 }
