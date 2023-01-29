c2e6f4ef2f93d86bf842b08befd303d21bb1c63d
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2023-01-29 19:30:16 +0600

    Gorgeous footer design completed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,58 @@
+import styles from "@/styles/Footer.module.css";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" styles={{ objectFit: "cover" }} fill alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            PEOPLE DISAPPOINT, BUT PIZZA NEVER DOES
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            146-G, Green Plaza.
+            <br /> Dhanmondi, R. 2/A
+            <br /> (683) 746-3011
+          </p>{" "}
+          <br />
+          <p className={styles.text}>
+            243-W, Water World.
+            <br /> Gulshan, R. 47/B
+            <br /> (683) 746-3012
+          </p>
+          <br />
+          <p className={styles.text}>
+            8934-N, BGB Headquarter.
+            <br /> Peelkhana, R. 347/S
+            <br /> (683) 746-3013
+          </p>
+          <br />
+          <p className={styles.text}>
+            78-E, Airport.
+            <br /> Dhaka, R. 23/N
+            <br /> (683) 746-3014
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 8:00 - 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 - 23:59
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
